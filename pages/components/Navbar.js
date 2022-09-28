395f54dc8d3d184d8ed0230f0c81ad181454faf1
Igor-Ranielli <igorranielli@hotmail.com>
2022-09-28 16:46:39 -0300

    Components and layout

---
+++ pages/components/Navbar.js
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <div>
+                <Image scr="/images/pokeball.png" width="30" height="30" alt="PokeNext"/>
+                <h1>Pokenext</h1>
+            </div>
+            <ul>
+                <li>
+                    <Link href="/"><a>Home</a></Link>
+                </li>
+                <li>
+                    <Link href="/about"><a>About</a></Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
